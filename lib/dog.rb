259ee0936911e0e995016368e3679ef5d3a79c9f
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-08 14:53:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,11 @@ attr_accessor :mood
 attr_reader :name
 
 
- def initialize(name, owner)
-  @name = name
-  @owner = Owner
-  @mood = "nervous"
-end
+ def initialize(name)
+   @name = name
+ 
+   @mood = "nervous"
+ end
 
 
 end
